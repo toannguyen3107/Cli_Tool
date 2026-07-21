62ed786e21b65c8ea5702b8fcba9b2502c994689
t0ann9uy3n <t0ann9uy3n@gmail.com>
2026-07-21 21:48:42 +0700

    feat: add installable frida tool package

---
+++ src/frida_tool/commands/devices.py
@@ -0,0 +1,9 @@
+from frida_tool.utils.adb_utils import run_adb_command, list_devices as list_devices_func
+from frida_tool.utils.decorator import header
+def add_parser(subparsers):
+    parser = subparsers.add_parser("devices", help="List all connected devices")
+    parser.set_defaults(func=list_devices)
+
+@header
+def list_devices(args):
+    list_devices_func()
---
+++ src/frida_tool/commands/reboot.py
@@ -0,0 +1,8 @@
+from frida_tool.utils.adb_utils import run_adb_command, select_device
+def add_parser(subparsers):
+    parser = subparsers.add_parser("reboot", help="Reboot the device")
+    parser.set_defaults(func=reboot_device)
+
+def reboot_device(args):
+    device = select_device()
+    device.shell("reboot")
